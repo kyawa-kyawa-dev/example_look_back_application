c16a606f919867149bee861fc5f4d6f7cf2d9ab2
kyawa-kyawa-dev <kawaharasou0216@gmail.com>
2025-10-07 21:28:42 +0900

    feat: 新規タグを登録できるようにした

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,10 +4,23 @@ class TagsController < ApplicationController
   end
 
   def new
-
+    @tag = Tag.new
   end
 
   def create
-    
+    attrs = permit_params.merge(user_id: current_user.id)
+    tags = Tag.new(attrs)
+
+    if tags.save
+      redirect_to tags_path, notice: "タグが作成されました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def permit_params
+    params.require(:tag).permit(:name, :color)
   end
 end
