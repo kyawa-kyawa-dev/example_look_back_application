ec072aaba811f4416ffc22bdfe0ba13ab085f582
kyawa-kyawa-dev <kawaharasou0216@gmail.com>
2025-10-14 00:14:45 +0900

    feat: ナレッジ作成時に、現在登録されているタグをパーシャルを用いて表示できるようにした

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -5,15 +5,18 @@ class KnowledgesController < ApplicationController
 
   def new
     @knowledge = Knowledge.new
+    @tags = Tag.where(user_id: current_user.id)
   end
 
   def create
-
+    puts "----------------------------"
+    puts "params: #{permit_params.inspect}"
   end
 
   private
 
   def permit_params
-    params.require(:knowledge).permit(:title, :content, :urls)
+    # 今回のurlsのように値が配列で送信されている際には、配列形式で取得する必要があるらしい
+    params.require(:knowledge).permit(:title, :content, urls: [])
   end
 end
