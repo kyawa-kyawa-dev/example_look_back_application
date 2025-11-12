3024c3e77591c93b84865f79e98e9c56588eb5f7
kyawa-kyawa-dev <kawaharasou0216@gmail.com>
2025-11-12 23:35:15 +0900

    feat: Rails標準搭載のredirect_back_or_toメソッドを参考に、View内で安全にリダイレクトできるカスタムヘルパーを作成した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # 以下は安全にRefererを使用するためのメソッド
+  def safe_referer_path(fallback: root_path)
+    referer = request.referer
+
+    return fallback if referer.blank?
+
+    uri = URI.parse(referer) rescue nil
+    return fallback unless uri
+
+    return uri.host == request.host ? referer : fallback
+  end
 end
